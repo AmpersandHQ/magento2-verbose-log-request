aa7f1a5f4ae7ba8e0a618a93c4085320ac4f082f
Luke Rodgers <lr@amp.co>
2022-11-30 20:14:57 +0000

    Update tests

--- src/Test/Integration/RedisLogLevelTest.php
+++ src/Test/Integration/RedisLogLevelTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Ampersand\VerboseLogRequest\Test\Integration;
 
+use Ampersand\VerboseLogRequest\Service\IsVerbose\Storage;
 use Magento\Framework\Session\SaveHandler\Redis\Config as RedisConfig;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
@@ -12,13 +13,14 @@ class RedisLogLevelTest extends TestCase
     public function testGetDefaultLogLevel()
     {
         $redisConfig = Bootstrap::getObjectManager()->get(RedisConfig::class);
-        $this->assertEquals('asdf', $redisConfig->getLogLevel());
+        $this->assertNull($redisConfig->getLogLevel(), 'Default log level is not null');
     }
 
     public function testGetVerboseLogLevel()
     {
-        $this->markTestSkipped('TODO');
+        Storage::setFlag(true, true);
         $redisConfig = Bootstrap::getObjectManager()->get(RedisConfig::class);
-        $this->assertEquals('asdf', $redisConfig->getLogLevel());
+        $this->assertEquals('7', $redisConfig->getLogLevel(), 'Default log level is not 7');
+        Storage::reset(true);
     }
 }
